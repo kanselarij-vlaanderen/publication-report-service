5e0884863261f4846bc9d63ad35f859170d9f2da
mdv-professional <mdv.werk@outlook.com>
2022-03-21 14:17:43 +0100

    try-out: download query to CSV KAS-3179 pub: v3: reports: basic

--- app.js
+++ app.js
@@ -1,10 +1,15 @@
 import mu from 'mu';
+import * as SparqlDownloadClient from './lib/SparqlDownloadClient.js';
 import * as RegulationType from './queries/reports/RegulationType.js';
 
 mu.app.get('/', async function (req, res, next) {
   try {
-    const q = await RegulationType.query();
-    res.send(JSON.stringify(await mu.query(q)));
+    const query = await RegulationType.query();
+    await SparqlDownloadClient.toCsv(
+      query,
+      `/app/data/test${new Date().toISOString().slice(0, 10)}.csv`
+    );
+    res.send(JSON.stringify({ status: 'OK' }));
   } catch (err) {
     next(err, req, res, next);
   }
