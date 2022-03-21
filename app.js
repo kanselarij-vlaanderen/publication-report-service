8d61d01603e233ee00f53be4467574996696aac9
mdv-professional <mdv.werk@outlook.com>
2022-03-21 14:51:06 +0100

    fine-tune: download query to CSV: - extract virtuoso sparql endpoint KAS-3179 pub: v3: reports: basic

--- app.js
+++ app.js
@@ -2,10 +2,14 @@ import mu from 'mu';
 import * as VirtuosoClient from './lib/VirtuosoClient.js';
 import * as RegulationType from './queries/reports/RegulationType.js';
 
+const client = VirtuosoClient.create({
+  sparqlEndpointUrl: process.env.VIRTUOSO_SPARQL_ENDPOINT,
+});
+
 mu.app.get('/', async function (req, res, next) {
   try {
     const query = await RegulationType.query();
-    await VirtuosoClient.toCsv(
+    await client.downloadToCsv(
       query,
       `/app/data/test${new Date().toISOString().slice(0, 10)}.csv`
     );
