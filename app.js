85f948e5fd62c68f39d793df0e1058567c1a28c8
mdv-professional <mdv.werk@outlook.com>
2022-03-22 12:09:49 +0100

    try-out: listen to deltas KAS-3179 pub: v3: reports: basic

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ import mu from 'mu';
 import * as config from './config.js';
 import * as VirtuosoClient from './lib/VirtuosoClient.js';
 import * as RegulationType from './queries/reports/RegulationType.js';
+import bodyParser from 'body-parser';
 
 const client = VirtuosoClient.create({
   sparqlEndpointUrl: config.VIRTUOSO_SPARQL_ENDPOINT,
@@ -20,6 +21,28 @@ mu.app.get('/', async function (req, res, next) {
   } catch (err) {
     next(err, req, res, next);
   }
+});
+
+mu.app.post('/delta', bodyParser.json(), async function (req, res, next) {
+  res.status(202).end();
+
+  let deltas = req.body;
+  let newJobUris = [];
+  for (let delta of deltas) {
+    let inserts = delta.inserts;
+    for (let triple of inserts) {
+      let isJob =
+        triple.predicate.value === 'http://www.w3.org/1999/02/22-rdf-syntax-ns#type' &&
+        triple.object.value === 'http://mu.semte.ch/vocabularies/ext/publicatie/PublicationMetricsExportJob';
+
+      if (isJob) {
+        let jobUri = triple.subject.value;
+        newJobUris.push(jobUri);
+      }
+    }
+  }
+
+  console.log("I received jobs with URI: ", newJobUris.join(' '))
 });
 
 mu.app.use(mu.errorHandler);
