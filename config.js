f984d7b88ff34e8e2756976f3cfa38f092cef53f
mdv-professional <mdv.werk@outlook.com>
2022-03-30 13:12:06 +0200

    throw meaningful error when VIRTUOSO_SPARQL_ENDPOINT env var not set KAS-3179 pub: v3: reports: basic

--- config.js
+++ config.js
@@ -1,2 +1,6 @@
 export const { VIRTUOSO_SPARQL_ENDPOINT } = process.env;
+if (!VIRTUOSO_SPARQL_ENDPOINT) {
+  throw new Error('missing environment variable: VIRTUOSO_SPARQL_ENDPOINT');
+}
+
 export const STORAGE_PATH = `/share`;
