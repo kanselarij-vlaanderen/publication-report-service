1a16207211d8cdbbace7e1226d145ec3536118ee
mdv-professional <mdv.werk@outlook.com>
2022-04-05 15:53:57 +0200

    use Express functions: - use json parser included in Express - sendStatus method KAS-3179 pub: v3: reports: basic

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
+import * as Express from 'express';
 import Mu from 'mu';
 import * as JobRunner from './lib/job-runner.js';
 import * as DownloadJob from './lib/download-job.js';
 import * as Delta from './lib/delta.js';
-import bodyParser from 'body-parser';
 
-Mu.app.post('/delta', bodyParser.json(), async function (req, res) {
-  res.status(202).end();
+Mu.app.post('/delta', Express.json(), async function (req, res) {
+  res.sendStatus(202);
 
   let deltas = req.body;
   let newJobUris = Delta.filterInsertedJobUris(deltas);
