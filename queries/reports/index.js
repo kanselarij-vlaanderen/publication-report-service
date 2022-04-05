135111729e3ffddcf05c198df01d9a14c4296d25
mdv-professional <mdv.werk@outlook.com>
2022-04-05 12:35:55 +0200

    (rf:extract method) reports query KAS-3179 pub: v3: reports: basic

--- queries/reports/index.js
+++ queries/reports/index.js
@@ -2,4 +2,14 @@ import * as GovernmentDomain from './government-domain.js';
 import * as Mandatee from './mandatee.js';
 import * as RegulationType from './regulation-type';
 
-export { GovernmentDomain, Mandatee, RegulationType };
+export function build(params) {
+  let sparqlQuery;
+  if (params.group === 'government-domain') {
+    sparqlQuery = GovernmentDomain.build(params);
+  } else if (params.group === 'mandatee') {
+    sparqlQuery = Mandatee.build(params);
+  } else if (params.group === 'regulation-type') {
+    sparqlQuery = RegulationType.build(params);
+  }
+  return sparqlQuery;
+}
